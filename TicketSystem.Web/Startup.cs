fbef7463c3e0b6b98a4a17c171293ded88ae7be0
mickey.shiue <e031304@gmail.com>
2022-05-04 20:46:11 +0800

    refactor：調整Code

--- TicketSystem.Web/Startup.cs
+++ TicketSystem.Web/Startup.cs
@@ -15,7 +15,7 @@ namespace TicketSystem.Web
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
+        private IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
